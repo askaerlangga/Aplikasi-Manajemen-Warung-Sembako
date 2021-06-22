892f663d5799e935602b6c0a5fa6e6d1bfbc451f
xcybermrx08 <66215532+xcybermrx08@users.noreply.github.com>
2021-06-22 16:10:52 +0700

    terbarulagi

---
+++ Aplikasi-Manajemen-Warung-Sembako/ModuleKoneksi.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.Odbc
+
+Module ModuleKoneksi
+
+    Public conn As OdbcConnection
+    Public cmd As OdbcCommand
+    Public da As OdbcDataAdapter
+    Public dr As OdbcDataReader
+    Public ds As DataSet
+    Public dt As New DataTable
+    Public str As String
+    Public x As Integer
+
+    Sub konek()
+        Dim lokasidata As String
+
+        lokasidata = "Driver={MySQL ODBC 3.51 Driver};Database=databasewarungkito;server=localhost;uid=root"
+        conn = New OdbcConnection(lokasidata)
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        Else
+
+        End If
+    End Sub
+
+End Module
