301a353696154b8def14ca38dcf607cc3109d031
xcybermrx08 <66215532+xcybermrx08@users.noreply.github.com>
2021-06-24 23:47:04 +0700

    Update dbwarungkito.sql

--- database/dbwarungkito.sql
+++ database/dbwarungkito.sql
@@ -1,17 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Waktu pembuatan: 24 Jun 2021 pada 09.44
+-- Waktu pembuatan: 24 Jun 2021 pada 18.46
 -- Versi server: 10.4.18-MariaDB
 -- Versi PHP: 7.3.27
-=======
--- Generation Time: Jun 23, 2021 at 06:56 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
->>>>>>> d27240243b766b40c0bb9d299926671d40b85717
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gudang`
+-- Struktur dari tabel `gudang`
 --
 
 CREATE TABLE `gudang` (
@@ -42,35 +36,22 @@ CREATE TABLE `gudang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `gudang`
+-- Dumping data untuk tabel `gudang`
 --
 
 INSERT INTO `gudang` (`nogudang`, `NamaBarang`, `jumlah`, `harga`, `satuan`) VALUES
-('B002\r\n', 'Mie Rebus', 100, 3000, 'pcs');
+('B001', 'Indomie Goreng', 100, 2500, 'pcs'),
+('B002', 'Indomie Ayam Barang', 100, 3000, 'pcs'),
+('B003', 'Sunlight', 100, 1000, 'pcs'),
+('B004', 'Beras Pandan', 100, 7000, 'liter'),
+('B005', 'Minyak', 100, 12000, 'liter'),
+('B006', 'Gula', 100, 14000, 'kg'),
+('B007', 'Telur', 200, 1500, 'pcs');
 
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
--- Struktur dari tabel `tbl_pelanggan`
---
-
-CREATE TABLE `tbl_pelanggan` (
-  `nopelanggan` varchar(10) NOT NULL,
-  `namapelanggan` varchar(50) NOT NULL,
-  `alamatpelanggan` varchar(100) NOT NULL,
-  `notelp` varchar(12) NOT NULL,
-  `hutang` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `tbl_pelanggan`
---
-
-INSERT INTO `tbl_pelanggan` (`nopelanggan`, `namapelanggan`, `alamatpelanggan`, `notelp`, `hutang`) VALUES
-('P001', 'Aska Erlangga', 'Komplek BAP', '089765372645', 50000);
-=======
--- Table structure for table `tabeluser`
+-- Struktur dari tabel `tabeluser`
 --
 
 CREATE TABLE `tabeluser` (
@@ -85,36 +66,85 @@ CREATE TABLE `tabeluser` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabeluser`
+-- Dumping data untuk tabel `tabeluser`
 --
 
 INSERT INTO `tabeluser` (`kode_user`, `username_user`, `password_user`, `nama_user`, `alamat_user`, `no_telp_user`, `jabatan_user`, `jenis_user`) VALUES
-('USER001', 'admin', 'admin', 'Admin', 'Kota Serang', '08123456789', 'Administrator', 'admin'),
-('USER002', 'aska', '123456', 'Aska Erlangga', 'Kota Serang', '08123456789', 'Kuli', 'user');
->>>>>>> d27240243b766b40c0bb9d299926671d40b85717
+('K002', 'edofr', '203387123', 'edo fitri rifai', 'komplek rsspemda', '08960626752431', 'admin', 'admin'),
+('K001', 'edofr', '203387123', 'edo fitri rifai', 'komplek rsspemda', '08960626752431', 'admin', 'admin'),
+('K004', '', '', '', '', '', '', ''),
+('K004', 'fajarf', '12345', 'Fajar Nurfauzan', 'Pamarayan', '09239017', 'Gudang', 'user'),
+('K005', 'askae', '12345', 'Aska Erlangga', 'Komplek BAP', '08978074874', 'kasir', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `nonota` varchar(10) NOT NULL,
+  `namabarang` varchar(50) NOT NULL,
+  `qty` int(10) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `total` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_detailbarang`
+--
+
+CREATE TABLE `tbl_detailbarang` (
+  `nonota` varchar(10) NOT NULL,
+  `tglbarang` date NOT NULL,
+  `itembarang` int(10) NOT NULL,
+  `totaljual` int(10) NOT NULL,
+  `dibayar` int(10) NOT NULL,
+  `kembali` int(10) NOT NULL,
+  `jenis_user` varchar(50) NOT NULL,
+  `kode_user` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pelanggan`
+--
+
+CREATE TABLE `tbl_pelanggan` (
+  `nopelanggan` varchar(10) NOT NULL,
+  `namapelanggan` varchar(50) NOT NULL,
+  `alamatpelanggan` varchar(100) NOT NULL,
+  `notelp` varchar(12) NOT NULL,
+  `hutang` int(10) NOT NULL,
+  `keterangan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_pelanggan`
+--
+
+INSERT INTO `tbl_pelanggan` (`nopelanggan`, `namapelanggan`, `alamatpelanggan`, `notelp`, `hutang`, `keterangan`) VALUES
+('p002', 'edofr', 'komplek rsspemda', '089606267524', 0, 'LUNAS');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `gudang`
+-- Indeks untuk tabel `gudang`
 --
 ALTER TABLE `gudang`
   ADD PRIMARY KEY (`nogudang`);
 
 --
-<<<<<<< HEAD
 -- Indeks untuk tabel `tbl_pelanggan`
 --
 ALTER TABLE `tbl_pelanggan`
   ADD PRIMARY KEY (`nopelanggan`);
-=======
--- Indexes for table `tabeluser`
---
-ALTER TABLE `tabeluser`
-  ADD PRIMARY KEY (`kode_user`);
->>>>>>> d27240243b766b40c0bb9d299926671d40b85717
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
